120048fe35a9dbe0ef2e7485806bdf95fbcd96c8
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-07-05 11:59:06 -0300

    added testc= for classroom file

--- test.rb
+++ test.rb
@@ -3,6 +3,8 @@ require_relative 'book'
 require_relative 'person'
 require_relative 'rental'
 require_relative 'decorator'
+require_relative 'classroom'
+require_relative 'student'
 require 'date'
 
 
@@ -101,6 +103,34 @@ RSpec.describe TrimmerDecorator do
         allow(nameable).to receive(:correct_name).and_return('John Doe')
         expect(subject.correct_name).to eq('John Doe')
       end
+    end
+  end
+end
+
+RSpec.describe Classroom do
+  let(:classroom_label) { 'Mathematics' }
+  let(:classroom) { described_class.new(classroom_label) }
+  let(:student) { Student.new('John Doe') }
+
+  describe '#initialize' do
+    it 'sets the classroom label' do
+      expect(classroom.label).to eq(classroom_label)
+    end
+
+    it 'initializes an empty students array' do
+      expect(classroom.students).to be_empty
+    end
+  end
+
+  describe '#add_student' do
+    it 'adds a student to the students array' do
+      classroom.add_student(student)
+      expect(classroom.students).to include(student)
+    end
+
+    it 'sets the classroom for the added student' do
+      classroom.add_student(student)
+      expect(student.classroom).to eq(classroom)
     end
   end
 end
